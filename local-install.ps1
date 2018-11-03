5c713229fe8975e4271eec910a1776733f7d8cc6
ChrististianGlessner <ChrististianGlessner@DESKTOP-0SM8PGN>
2018-11-03 14:59:13 +0100

    Version 0.0.2

--- local-install.ps1
+++ local-install.ps1
@@ -1,4 +1,4 @@
 & .\build.ps1
 
-dotnet new -i ".\Bin\Ltwlf.BotBuilderV4.Basic.0.0.1.nupkg"
-dotnet new -i ".\Bin\Ltwlf.BotBuilderV4.Echo.0.0.1.nupkg"
+dotnet new -i ".\Bin\Ltwlf.BotBuilderV4.Basic.0.0.2.nupkg"
+dotnet new -i ".\Bin\Ltwlf.BotBuilderV4.Echo.0.0.2.nupkg"
